7843aa565b330cc97620e24aa9e5c62c8f9d2b1c
furkanOzkannN <seninmailin@ornek.com>
2025-12-07 23:11:00 +0300

    Initial commit

--- obj/Debug/net8.0/Payona.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Payona.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Payona.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1ebcb2ef90cff52730d28711805a90f65bfbcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945ec7e19db3f00610889a5dba293358435f8cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Payona.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Payona.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
